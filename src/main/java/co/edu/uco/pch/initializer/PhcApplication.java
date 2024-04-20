effcd95b753cdbb7a15513881b5d8a5daf0ecfc5
aarbelaez15 <144059940+aarbelaez15@users.noreply.github.com>
2024-04-19 20:08:58 -0500

    Add files via upload

---
+++ src/main/java/co/edu/uco/pch/initializer/PhcApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.pch.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uco/pch/initializer/PhcApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.pch.initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
