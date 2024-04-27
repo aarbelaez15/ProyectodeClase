6a4ff0ccf6a9ed68f17866ef7790164f87d5db6a
aarbelaez15 <144059940+aarbelaez15@users.noreply.github.com>
2024-04-26 23:05:02 -0500

    Add files via upload

---
+++ src/main/java/co/edu/uco/pch/crosscutting/helpers/exceptions/customs/DataPCHException.java
@@ -0,0 +1,8 @@
+package co.edu.uco.pch.crosscutting.helpers.exceptions.customs;
+
+import co.edu.uco.pch.crosscutting.exceptions.PCHException;
+import co.edu.uco.pch.crosscutting.exceptions.PCHException;
+
+public class DataPCHException {
+
+}
